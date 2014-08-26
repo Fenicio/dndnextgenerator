570fd0a7637ae3b37539216526027b992eb9e8c2
GG Tipo <scorpid@gmail.com>
2014-08-26 11:54:47 +0000

    Nearing insertion and modification

--- client/types/type_edit.js
+++ client/types/type_edit.js
@@ -14,8 +14,53 @@ Template.type_edit.events({
     'click [name="xtra_trait"]': function(e) {
         UI.insert(UI.render(Template.trait_form), $('[name="traits"]')[0]);
     },
-    'submit': function(e) {
+    'click [name="save"]': function(e) {
         e.preventDefault();
-        console.log(e);
+        var type = {
+            name: $('[name="name"]').val(),
+            type: $('[name="type"]').val(),
+            traits: [],
+            actions: [],
+            skills: [],
+            stats: {
+                STR: $('[name="STR"]').val(),
+                DEX: $('[name="DEX"]').val(),
+                CON: $('[name="CON"]').val(),
+                INT: $('[name="INT"]').val(),
+                WIS: $('[name="WIS"]').val(),
+                CHA: $('[name="CHA"]').val()
+            },
+            natural_armor: $('[name="natural_armor"]').val(),
+            adjetives: $('[name="adjetives"]').val(),
+            prefix: $('[name="prefix"]').val(),
+            suffix: $('[name="suffix"]').val()
+        };
+        console.log(type);
+        $('[name="actions"]').children().each(function(el, i) {
+            var action = {
+                name: el.find('[name="action_name"]').val(),
+                action: el.find('[name="action_action"]').val(),
+                hitBonus: el.find('[name="action_hitbonus"]').val(),
+                text: el.find('[name="action_text"]').val()
+            };
+            type.actions.push(action);
+        });
+        $('[name="skills"]').children().each(function(el, i) {
+            var skill = {
+                name: el.find('[name="skill_name"]').val(),
+                value: el.find('[name="skill_value"]').val() 
+            };
+            types.skills.push(skill);
+        });
+        $('[name="traits"]').children().each(function(el, i) {
+            var trait = {
+                name: el.find('[name="trait_name"]').val(),
+                text: el.find('[name="trait_text"]').val()
+            };
+            type.traits.push(trait);
+        });
+        Meteor.call('type', type, function(error, typeId) {
+            console.log(error, typeId);
+        });
     }
 });
--- collections/types.js
+++ collections/types.js
@@ -3,6 +3,26 @@ Attacks on types can have these descriptors/templates:
 {{min-damage}}
 {{avg-damage}}
 {{max-damage}}
+
+{ name: '',
+   type: 'main',
+   traits: [],
+   actions: [],
+   skills: [],
+   stats: { STR: '', DEX: '', CON: '', INT: '', WIS: '', CHA: '' },
+   natural_armor: '',
+   adjetives: '',
+   prefix: '',
+   suffix: '' }
+   
 */
 
 Types = new Meteor.Collection('types');
+
+Meteor.methods({
+    type: function(typeAttr) {
+        console.log(typeAttr);
+    
+        return typeAttr;
+    }
+});
